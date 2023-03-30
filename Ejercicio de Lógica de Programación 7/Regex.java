72ab76ba0a9507890612a574af07f7b26ba0481f
asgarynt <116381396+asgarynt@users.noreply.github.com>
2023-03-30 15:46:16 -0600

    Update Regex.java
    Se actualiza el regex de hora y minutos que estaba incorrecto.

--- Ejercicio de Lógica de Programación 7/Regex.java
+++ Ejercicio de Lógica de Programación 7/Regex.java
@@ -1,7 +1,7 @@
 public class Regex {
 
-    String hourPattern = "([0-2]?[0-4])";
-    String minutesPattern = "([0-5]?[0-9]|[0-6]?[0])";
+    String hourPattern = "(([0-1]?[0-9])|(2[0-4]))";
+    String minutesPattern = "([0-5]?[0-9]|60)";
 
     String diaPattern = "\\b(lunes|martes|miercoles|jueves|viernes)\\b";
 }
